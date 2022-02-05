3663e60f26ece52b67e850ee05cebe1e24072261
Noah Lee <noah.lee@buzzvil.com>
2022-02-05 17:20:12 +0900

    Add the index page for configurations (#20)
    * Add the index page for configurations
    
    * Fix the edit URL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,14 +38,12 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/gitploy-io/website/tree/main/',
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/gitploy-io/website/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
