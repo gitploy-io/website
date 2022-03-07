717ad0ceb79ced4351a1fd8e8aae00a16be638ab
noah <noah.lee@buzzvil.com>
2022-03-07 23:28:32 +0900

    Replace the home page with the demo

---
+++ src/components/HomepageDemo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './HomepageDemo.module.css';
+
+export default function HomepageDemo() {
+  return (
+    <section className="padding-vert--xl">
+      <div className="container">
+        <div className="text--center">
+          <h2>BUILD A POWERFUL DEPLOYMENT SYSTEM</h2>
+          <p>Gitploy provides a fast, safe and secure deployment pipeline to your organization. <br/> Let's build your deployment system in minutes.</p>
+          <img className={clsx(styles.image, 'shadow--md', )} src={require('../../static/img/docs/gitploy-v3.gif').default}/>
+        </div>
+      </div>
+    </section>
+  )
+}
