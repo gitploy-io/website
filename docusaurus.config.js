07506c52ea6a6b954463e7637bc41728c77eef2a
Noah Lee <noah.lee@buzzvil.com>
2022-03-10 22:07:05 +0900

    Add an article for multiple deployment (#40)
    * Add an article for multiple environments
    
    * Add metadatas
    
    * Add the description meta

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -97,7 +97,7 @@ const config = {
             position: 'left',
             label: 'Docs',
           },
-          // {to: '/blog', label: 'Blog', position: 'left'},
+          {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/gitploy-io/gitploy',
             position: 'right',
@@ -134,10 +134,10 @@ const config = {
           {
             title: 'More',
             items: [
-              // {
-              //   label: 'Blog',
-              //   to: '/blog',
-              // },
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
               {
                 label: 'Cloud',
                 href: 'https://cloud.gitploy.io',
