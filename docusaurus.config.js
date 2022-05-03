ee145b118946ec7ab1cc43b417decf2dbffdb66f
noah <noah.lee@buzzvil.com>
2022-05-03 22:39:47 +0900

    Upgrade docusaurus

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -158,16 +158,6 @@ const config = {
       colorMode: {
         defaultMode: 'light',
         respectPrefersColorScheme: true,
-        switchConfig: {
-          darkIcon: '🌙',
-          darkIconStyle: {
-            marginLeft: '2px',
-          },
-          lightIcon: '☀️',
-          lightIconStyle: {
-            marginLeft: '1px',
-          },
-        },
       },
       prism: {
         theme: lightCodeTheme,
